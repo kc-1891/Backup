f1e22224d6caf6ed6e16eb3820340351d5dd1d7d
Kumaran <kumaran.cb@gmail.com>
2018-02-26 19:07:17 +0000

    Second commit

---
+++ 2.py
@@ -0,0 +1,20 @@
+# def input_age(age):
+#     new_age = age + 50
+#     return new_age
+#
+#
+# fetch_age = int(input('Enter the age: '))
+# print(input_age(fetch_age))
+
+
+# def convert_fahrenheit(celsius):
+#     f = celsius * 9/5 + 32
+#     return f
+# celsius = float(input('Enter the celsius: '))
+# print(convert_fahrenheit(celsius))
+
+def find_len(str):
+    return len(str)
+
+fetch_str = input('Enter the string: ')
+print(find_len(fetch_str))
